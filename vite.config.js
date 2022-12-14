fee1ef1a907b3564947189b38ff9b2e9fad48d58
belopot <belopot@protonmail.com>
2022-12-14 21:52:31 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,26 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import glsl from 'vite-plugin-glsl';
 import jsconfigPaths from 'vite-jsconfig-paths';
+import metaversefilePlugin from 'metaversefile/plugins/rollup.js';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 4000,
-  },
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  assetsInclude: ['**/*.glb', '**/*.hdr'],
-  plugins: [glsl(), react(), jsconfigPaths()],
+export default defineConfig(command => {
+  return {
+    server: {
+      fs: {
+        strict: true,
+      },
+    },
+    esbuild: {
+      loader: 'jsx',
+      include: /src\/.*\.jsx?$/,
+      exclude: [],
+    },
+    assetsInclude: ['**/*.glb', '**/*.hdr'],
+    plugins: (command === 'build' ? [] : [metaversefilePlugin()]).concat([
+      glsl(),
+      react(),
+      jsconfigPaths(),
+    ]),
+  };
 });

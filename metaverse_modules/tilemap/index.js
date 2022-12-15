eb87a1e3e4300029b88d5addcc1e98112d7a2511
alextitonis <alexandrostitonis@gmail.com>
2022-12-15 15:57:54 +0200

    updated sprites

--- metaverse_modules/tilemap/index.js
+++ metaverse_modules/tilemap/index.js
@@ -254,28 +254,11 @@ export default e => {
     })(),
   );
 
-  // add physics
-  const geometry = new THREE.PlaneGeometry(0.01, 0.01);
-  geometry.rotateY(Math.PI / 2); // note: match with physx' default plane rotation.
-  const material = new THREE.MeshStandardMaterial({color: 'red'});
-  const physicsPlane = new THREE.Mesh(geometry, material);
-  physicsPlane.rotation.set(0, 0, Math.PI / 2);
-  app.add(physicsPlane);
-  physicsPlane.updateMatrixWorld();
-
-  const physicsObject = physics.addPlaneGeometry(
-    new THREE.Vector3(0, 0, 0),
-    new THREE.Quaternion().setFromEuler(new THREE.Euler(0, 0, Math.PI / 2)),
-    false,
-  );
-
   useFrame(() => {
     frameCb && frameCb();
   });
 
-  useCleanup(() => {
-    physics.removeGeometry(physicsObject);
-  });
+  useCleanup(() => {});
 
   return app;
 };

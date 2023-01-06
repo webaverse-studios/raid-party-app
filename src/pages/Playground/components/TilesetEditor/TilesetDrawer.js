07ac5a14fccfa0db89f58a8dd47712c38a8d4152
belopot <belopot@protonmail.com>
2023-01-06 16:41:10 +0800

    Set tile size

--- src/pages/Playground/components/TilesetEditor/TilesetDrawer.js
+++ src/pages/Playground/components/TilesetEditor/TilesetDrawer.js
@@ -1,6 +1,12 @@
+const GRID_COLOR = 'rgba(0,255,217,0.7)';
+
 export default class TilesetDrawer {
   canvas;
   ctx;
+  image = null;
+  tileSize = 32;
+  imageWidth = 60;
+  imageHeight = 60;
   /**
    * @param {HTMLElement} canvas
    */
@@ -9,33 +15,40 @@ export default class TilesetDrawer {
     this.ctx = canvas.getContext('2d');
   }
 
-  /**
-   * @param {HTMLElement} image
-   */
-  drawImage(image) {
-    // set canvs size
-    this.canvas.width = image.naturalWidth;
-    this.canvas.height = image.naturalHeight;
-
-    // draw image
-    this.ctx.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight);
-
-    // draw grid
-    this.drawGrid(image.naturalWidth, image.naturalHeight);
+  setDrawValues(image, tileSize) {
+    this.image = image;
+    this.imageWidth = image.naturalWidth;
+    this.imageHeight = image.naturalHeight;
+    this.tileSize = tileSize;
   }
 
-  drawGrid = (w, h, step = 32, color = 'rgba(0,255,217,0.7)') => {
-    this.ctx.strokeStyle = color;
+  resizeCanvas() {
+    this.canvas.width = this.imageWidth;
+    this.canvas.height = this.imageHeight;
+  }
+
+  drawImage() {
+    this.ctx.drawImage(this.image, 0, 0, this.imageWidth, this.imageHeight);
+  }
+
+  drawGrid = () => {
+    this.ctx.strokeStyle = GRID_COLOR;
     this.ctx.lineWidth = 0.5;
     this.ctx.beginPath();
-    for (let x = 0; x < w + 1; x += step) {
+    for (let x = 0; x < this.imageWidth + 1; x += this.tileSize) {
       this.ctx.moveTo(x, 0.5);
-      this.ctx.lineTo(x, h + 0.5);
+      this.ctx.lineTo(x, this.imageHeight + 0.5);
     }
-    for (let y = 0; y < h + 1; y += step) {
+    for (let y = 0; y < this.imageHeight + 1; y += this.tileSize) {
       this.ctx.moveTo(0, y + 0.5);
-      this.ctx.lineTo(w, y + 0.5);
+      this.ctx.lineTo(this.imageWidth, y + 0.5);
     }
     this.ctx.stroke();
+  };
+
+  drawAll = () => {
+    this.resizeCanvas();
+    this.drawImage();
+    this.drawGrid();
   };
 }
--- src/pages/Playground/components/TilesetEditor/TilesetEditor.jsx
+++ src/pages/Playground/components/TilesetEditor/TilesetEditor.jsx
@@ -2,6 +2,7 @@ import {motion} from 'framer-motion';
 import React, {useContext, useEffect, useRef, useState} from 'react';
 import styled from 'styled-components';
 import {Dropdown} from 'primereact/dropdown';
+import {InputNumber} from 'primereact/inputnumber';
 
 import {AppContext} from '../../../../App';
 import TilesetDrawer from './TilesetDrawer';
@@ -44,27 +45,42 @@ export default function TilesetEditor() {
 
   const [imageLoading, setImageLoading] = useState(false);
   const [tileset, setTileset] = useState(null);
+  const [tileSize, setTileSize] = useState(32);
+  const [img, setImg] = useState(null);
+
   const canvasRef = useRef();
   const drawerRef = useRef();
 
+  // Init drawer
   useEffect(() => {
     if (canvasRef.current) {
       drawerRef.current = new TilesetDrawer(canvasRef.current);
     }
   }, [canvasRef.current]);
 
+  // Change tileset
   useEffect(() => {
     if (drawerRef.current && tileset) {
       setImageLoading(true);
       const image = new Image();
       image.onload = () => {
-        drawerRef.current.drawImage(image);
+        setImg(image);
+        drawerRef.current.setDrawValues(image, tileSize);
+        drawerRef.current.drawAll();
         setImageLoading(false);
       };
       image.src = tileset.url;
       image.crossOrigin = 'Anonymous';
     }
   }, [tileset, drawerRef.current]);
+
+  // Change tile size
+  useEffect(() => {
+    if (drawerRef.current && img) {
+      drawerRef.current.setDrawValues(img, tileSize);
+      drawerRef.current.drawAll();
+    }
+  }, [tileSize]);
 
   return (
     <Holder
@@ -87,13 +103,26 @@ export default function TilesetEditor() {
     >
       <Content>
         <Dropdown
-          className="w-full mb-2"
+          className="w-full mb-3"
           optionLabel="name"
           value={tileset}
           options={TILESETS}
           onChange={e => setTileset(e.value)}
           placeholder="Select a tileset"
         />
+        <div className="w-full mb-3">
+          <Label htmlFor="tilesize">Tile size</Label>
+          <InputNumber
+            className="w-full"
+            inputId="tilesize"
+            value={tileSize}
+            onValueChange={e => {
+              setTileSize(e.value);
+            }}
+            min={8}
+            max={512}
+          />
+        </div>
         <CanvasHolder>
           <Loader visible={imageLoading} />
           <canvas ref={canvasRef} />
@@ -128,4 +157,8 @@ const CanvasHolder = styled.div`
   flex: 1;
   overflow: auto;
   background-color: #282828;
+`;
+
+const Label = styled.label`
+  line-height: 2;
 `;
